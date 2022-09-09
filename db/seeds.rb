af9b98e78f6828013778625aba765c5721eefab9
Charles Langlois <schok53@gmail.com>
2022-09-08 21:24:13 -0400

    Updated db/seeds.rb comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# TODO: create records from csv at url https://gist.github.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6
+# dump records url https://gist.github.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6 to file at db/pokemon.csv
 
 require 'csv'
 
